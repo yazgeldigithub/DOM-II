eb5e2b9cf380dc239284c5358bdbead9f0767129
yazgeldigithub <devster2016@gmail.com>
2021-04-23 21:26:31 -0500

    added mouseover eventListener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+let nav1 = document.querySelectorAll('a')
+nav1[0].addEventListener('click', (event) => {
+    alert('You Clicked on Home Button!')
+  })
+
+nav1[1].addEventListener('dblclick', (event) => {
+    nav1[1].style.color = 'orange'
+})
+
+nav1[2].addEventListener('mouseover', (event) => {
+    nav1[2].style.color = 'red'
+
+    // reset the color after a short delay
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 1000);
+    }, false);
+
